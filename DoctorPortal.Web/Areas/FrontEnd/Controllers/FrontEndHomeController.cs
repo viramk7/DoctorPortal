7733129ec97c5e136d1c2e7deae5f036987765fc
Viram Keshwala <viramk7@gmail.com>
2019-01-07 00:02:51 +0530

    Added the frontend in new area

---
+++ DoctorPortal.Web/Areas/FrontEnd/Controllers/FrontEndHomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoctorPortal.Web.Areas.FrontEnd.Controllers
+{
+    public class FrontEndHomeController : Controller
+    {
+        // GET: FrontEnd/FrontEndHome
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ DoctorPortal.Web/Areas/FrontEnd/FrontEndAreaRegistration.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace DoctorPortal.Web.Areas.FrontEnd
+{
+    public class FrontEndAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName => "FrontEnd";
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "FrontEnd_default",
+                "FrontEnd/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
