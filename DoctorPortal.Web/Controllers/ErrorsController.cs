7a9fbf9d5ba944c929c015b4a4f987c0cbd75cef
Viram Keshwala <viramk7@gmail.com>
2019-02-26 21:49:10 +0530

    Added the custom error handling

---
+++ DoctorPortal.Web/Controllers/ErrorsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoctorPortal.Web.Controllers
+{
+    public class ErrorsController : Controller
+    {
+        public ActionResult Index(Exception error)
+        {
+            return View();
+        }
+
+        public ActionResult HttpError404(Exception error)
+        {
+            return View();
+        }
+
+        public ActionResult HttpError500(Exception error)
+        {
+            return View();
+        }
+
+        public ActionResult General(Exception error)
+        {
+            return View();
+        }
+
+
+    }
+}
