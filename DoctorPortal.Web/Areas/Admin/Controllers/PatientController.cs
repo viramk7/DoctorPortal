298e00767d901a6a8cb1bfc6a3058ba6266ba889
Viram Keshwala <viramk7@gmail.com>
2019-03-07 21:05:16 +0530

    Email promotions partial work

--- DoctorPortal.Web/Areas/Admin/Controllers/PatientController.cs
+++ DoctorPortal.Web/Areas/Admin/Controllers/PatientController.cs
@@ -1,30 +1,21 @@
-﻿using DoctorPortal.Web.Areas.Admin.Controllers;
-using DoctorPortal.Web.Areas.Admin.Models.ViewModels;
+﻿using DoctorPortal.Web.Areas.Admin.Models.ViewModels;
 using DoctorPortal.Web.Areas.Admin.Services.Patient;
 using Kendo.Mvc;
 using Kendo.Mvc.Extensions;
 using Kendo.Mvc.UI;
 using log4net;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
-namespace DoctorPortal.Web.Controllers
+namespace DoctorPortal.Web.Areas.Admin.Controllers
 {
     public class PatientController : BaseController
     {
         private readonly ILog _logger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
         private readonly IPatientService _patientService;
-
-        //public PatientController()
-        //{
-
-        //}
-
+        
         public PatientController(IPatientService patientService)
         {
             _patientService = patientService;
---
+++ DoctorPortal.Web/Areas/Admin/Controllers/PromotionsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoctorPortal.Web.Areas.Admin.Controllers
+{
+    public class PromotionsController : BaseController
+    {
+        public ActionResult Index(string patients = "")
+        {
+            if (string.IsNullOrEmpty(patients))
+                return RedirectToAction(nameof(Index),nameof(PatientController));
+
+            ViewBag.Patients = patients;
+
+            return View();
+        }
+    }
+}
