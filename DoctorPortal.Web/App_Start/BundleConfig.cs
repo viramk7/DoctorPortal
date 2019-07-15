f12472adfe39741e10f807958f8a218a72c32a90
Viram Keshwala <viramk7@gmail.com>
2019-07-15 20:00:56 +0530

    testing bundling and minification

--- DoctorPortal.Web/App_Start/BundleConfig.cs
+++ DoctorPortal.Web/App_Start/BundleConfig.cs
@@ -7,27 +7,11 @@ namespace DoctorPortal.Web
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            // CSS
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
+            bundles.Add(new StyleBundle("~/Content/css-files").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
-
-            // ******************************** FRONT END  ***************************************** //
-
-            // CSS
 
             bundles.Add(new StyleBundle("~/Content/admincss").Include(
                 "~/Content/css/style.css",
